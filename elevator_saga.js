59b6b6ea5d1181be59aef13568c9fadd0be756f2
jonah sobol <jonah.sobol@gmail.com>
2016-05-12 15:08:23 -0700

    improvements

--- elevator_saga.js
+++ elevator_saga.js
@@ -1,33 +1,35 @@
 {
-    init: function(elevators, floors) {
+  init: function(elevators, floors) {
 
 
-            // var elevator = elevators[0]; // Let's use the first elevator
-        elevators.forEach(function(elevator){
+    elevators.forEach(function(elevator){
+
+      elevator.on("floor_button_pressed", function(floorNum) {
+        elevator.goToFloor(floorNum)
+      });
+
+      elevator.on("idle", function() {
+        elevator.goToFloor(4);
+      });
+
+      elevator.on("up_button_pressed"), function(floorNum) {
+        elevator.getPressedFloors
+      }
+
+    });
+
+    floors.forEach(function(floor){
+      floor.on("up_button_pressed", function(floorNum) {
+        elevator.goToFloor(floorNum)
+      });
+    });
 
 
-            // Whenever the elevator is idle (has no more queued destinations) ...
+  },
 
-            elevator.on("floor_button_pressed", function(floorNum) {
-                 elevator.goToFloor(floorNum)
-            });
-
-            elevator.on("idle", function() {
-                // let's go to all the floors (or did we forget one?)
-                elevator.goToFloor(4);
-            });
-
-           floor.on("up_button_pressed", function(floorNum) {
-            elevator.goToFloor(floorNum)
-    // Maybe tell an elevator to go to this floor?
-            })
-
-
-        });
-    },
-    update: function(dt, elevators, floors) {
-        // We normally don't need to do anything here
-    }
+  update: function(dt, elevators, floors) {
+    // We normally don't need to do anything here
+  }
 }
 
 
